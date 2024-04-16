9e8fddf74b8522a11cbaafcba3bbf0304216324e
Julio <jdelgadillo@uamv.edu.ni>
2024-04-16 11:27:33 -0600

    ejercicios funciones

--- arregloFlotante.c
+++ arregloFlotante.c
@@ -1,22 +1,15 @@
+/*Almacenar 6 numeros flotsntes*/
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char const *argv[])
 {
     float numbers[6];
+    numbers[0] = 12;
+    numbers[1] = 1.68;
+    numbers[2] = 2.680;
+    numbers[3] = 3.5;
+    numbers[4] = 1512.9985463255;
 
-    numbers[0]= 2.15;
-    numbers[1]= 434.435;
-    numbers[2]= 544.214;
-    numbers[3]= 65.53;
-    numbers[4]= 4333.124;
-    numbers[5]= 235.46652;
 
-    printf("Elemento # 1: " "%.2f\n", numbers[0]);
-    printf("Elemento # 2: " "%.2f\n", numbers[1]);
-    printf("Elemento # 3: " "%.2f\n", numbers[2]);
-    printf("Elemento # 4: " "%.2f\n", numbers[3]);
-    printf("Elemento # 5: " "%.2f\n", numbers[4]);
-    printf("Elemento # 6: " "%.2f\n", numbers[5]);
-
-    return 0;
+printf("Elemento #1 %f\n", numbers[0]);
 }
