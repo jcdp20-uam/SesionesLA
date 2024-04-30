86baa7b79c996530b36a33da66675c69e9389a3b
Julio <jdelgadillo@uamv.edu.ni>
2024-04-30 12:07:34 -0600

    Sesiones 30/04/24

---
+++ Sesion30424/arregloFlotante.c
@@ -0,0 +1,15 @@
+/*Almacenar 6 numeros flotsntes*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float numbers[6];
+    numbers[0] = 12;
+    numbers[1] = 1.68;
+    numbers[2] = 2.680;
+    numbers[3] = 3.5;
+    numbers[4] = 1512.9985463255;
+
+
+printf("Elemento #1 %f\n", numbers[0]);
+}
